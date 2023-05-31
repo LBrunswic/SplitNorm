1330ae20b13fe1b53522ada45cf745e76f89a854
leo <poker@brunswic.fr>
2023-05-31 13:58:30 +0800

    Correction of weights-score shapes Incomplete implementation of higher convolution flow

--- ConvolutionalKernel/base.py
+++ ConvolutionalKernel/base.py
@@ -15,28 +15,28 @@ class CommandedTransformedDistribution(tf.keras.Model):
         self._sample = _sample
         self.distribution_dim = distribution_dim
         self.command_dim = command_dim
+        self.reshape = tf.keras.layers.Reshape((-1,))
 
     @tf.function
     def score(self,sample_batch,command_batch):
-        return self._score(self.parametrized_transformed_distribution,sample_batch,command_batch)
+        return self._score(sample_batch,command_batch)
 
     def prob(self,sample_batch,command_batch):
-        return self._prob(self.parametrized_transformed_distribution,sample_batch,command_batch)
+        return self._prob(sample_batch,command_batch)
 
     def sample(self,command_batch):
-        return self._sample(self.parametrized_transformed_distribution,command_batch)
+        return self._sample(command_batch)
 
-    # @tf.function
     def train_step(self, weighted_sample_command_batch):
         weight_batch, sample_batch, command_batch = tf.split(
             weighted_sample_command_batch,[1,self.distribution_dim,-1],
             axis=-1
         )
-        weight_batch=tf.reshape(weight_batch,(-1,))
-
+        weight_batch=self.reshape(weight_batch)
         trainable_var = self.trainable_variables
         with tf.GradientTape() as tape:
-            scores = self.score(sample_batch,command_batch)
+            scores = self.reshape(self.score(sample_batch,command_batch))
+            print(scores.shape,weight_batch.shape)
             scores = weight_batch*scores
             loss = tf.reduce_mean(scores)
         grad = tape.gradient(loss, trainable_var)
@@ -60,6 +60,7 @@ class CommandedTransformedDistribution(tf.keras.Model):
         )
         plt.savefig(name)
 
+
 class FlowEnsemble(tf.keras.Model):
     def __init__(self, flow_family, command_dim=0,distribution_dim=2,name='Flowensemble'):
         super(FlowEnsemble, self).__init__(name=name)
@@ -71,15 +72,16 @@ class FlowEnsemble(tf.keras.Model):
         self.distribution_dim = distribution_dim
 
     def build_dist(self,base_distribution,*build_args,**build_kwargs):
+        transformed_distribution = tfd.TransformedDistribution(distribution=base_distribution, bijector=self.flow_family, *build_args, **build_kwargs)
 
-        def _score(transformed_distribution,sample_batch,command_batch):
-            print(transformed_distribution.__repr__)
+        @tf.function
+        def _score(sample_batch,command_batch):
             return -transformed_distribution.log_prob(
                 sample_batch,
                 bijector_kwargs =  {'command': command_batch},
             )
 
-        def _prob(transformed_distribution,sample_batch,command_batch,*args,**kwargs):
+        def _prob(sample_batch,command_batch,*args,**kwargs):
             return transformed_distribution.prob(
                 sample_batch,
                 *args,
@@ -87,7 +89,7 @@ class FlowEnsemble(tf.keras.Model):
                 **kwargs
             )
 
-        def _sample(transformed_distribution,command_batch,*args,**kwargs):
+        def _sample(command_batch,*args,**kwargs):
             sample_size = len(command_batch)
             weights = tf.ones((sample_size,1)) # (sample_size,1)
             samples = transformed_distribution.sample(
@@ -99,7 +101,7 @@ class FlowEnsemble(tf.keras.Model):
             return tf.concat([weights,samples],axis=1)
 
         return CommandedTransformedDistribution(
-            tfd.TransformedDistribution(distribution=base_distribution, bijector=self.flow_family, *build_args, **build_kwargs),
+            transformed_distribution,
             _score,
             _prob,
             _sample,
@@ -109,7 +111,7 @@ class FlowEnsemble(tf.keras.Model):
 
 
 class ConvKernel(tf.keras.Model):
-    def __init__(self, kernel_ensemble, channeller, commander, channel_dim=0, command_dim=0,name='ConvFlow'):
+    def __init__(self, kernel_ensemble, channeller, commander, channel_dim=0,distribution_dim=2, command_dim=0,name='ConvFlow'):
         super(ConvKernel, self).__init__(name=name)
         self.kernel_ensemble = kernel_ensemble
         self.channeller = channeller
@@ -123,18 +125,19 @@ class ConvKernel(tf.keras.Model):
 
     def build_dist(self,base_distribution):
         transformed_distribution = self.kernel_ensemble.build_dist(base_distribution)
-        def _score(transformed_distribution,sample_batch,command_batch,*args,**kwargs):
+        @tf.function
+        def _score(sample_batch,command_batch):
             weighted_channel_batch = self.channeller((sample_batch, command_batch))
             # (data_batch_size,command_batch_size,command_dim+1)
             A = [
-                self.reshape(transformed_distribution.score(sample_batch,  self.commander((channel_batch, command_batch)), *args,**kwargs))
+                self.reshape(transformed_distribution.score(sample_batch,  self.commander((channel_batch, command_batch))))
                 for channel_batch in tf.unstack(weighted_channel_batch[:, :, :-1], axis=1)
             ]
             scores = self.concat(A)
             weight_batch = weighted_channel_batch[:, :, -1]
             return self.dot([scores, weight_batch])
 
-        def _prob(transformed_distribution,sample_batch,command_batch,*args,**kwargs):
+        def _prob(sample_batch,command_batch,*args,**kwargs):
             weighted_channel_batch = self.channeller((sample_batch, command_batch))
             # (data_batch_size,command_batch_size,command_dim+1)
             A = [
@@ -145,7 +148,7 @@ class ConvKernel(tf.keras.Model):
             weight_batch = weighted_channel_batch[:, :, -1]
             return self.dot([probs, weight_batch])
 
-        def _sample(transformed_distribution,channel_command_batch,*args,**kwargs):
+        def _sample(channel_command_batch,*args,**kwargs):
             samples = transformed_distribution.sample(
                 self.commander(tf.split(channel_command_batch,[self.channel_dim,self.command_dim],axis=-1)),
                 *args,
@@ -163,73 +166,120 @@ class ConvKernel(tf.keras.Model):
         )
 
 
-class ConvFlowV2(tf.keras.Model):
-    """Multi-layer NN ode_fn."""
-    def __init__(self, flow, channel,quantization_dim, channel_kernel_dim,name='ConvFlow',
-                 base_distribution_gen = tfd.MultivariateNormalDiag,
-                 base_distributionKWarg = None
-                 ):
-        super(ConvFlowV2, self).__init__(name=name)
-        self.flow = flow
-        self.channel = channel
-        self.quantization_dim = quantization_dim
-        self.channel_kernel_dim = channel_kernel_dim
-        self.base_distribution_gen = base_distribution_gen
-        self.base_distributionKWarg = base_distributionKWarg
-        self.reshape = tf.keras.layers.Reshape((1,1))
-        self.concat_prob = tf.keras.layers.Concatenate(axis=-2)
-        self.reshape_total = tf.keras.layers.Reshape((self.quantization_dim,1))
-        self.concat_total = tf.keras.layers.Concatenate(axis=-1)
-        self.reshape_command = tf.keras.layers.Reshape((1,-1))
-        self.dot = tf.keras.layers.Dot(axes=-1)
-        self.base_distribution = self.base_distribution_gen(**self.base_distributionKWarg)
-        self.transformed_distribution = tfd.TransformedDistribution(distribution=self.base_distribution, bijector=self.flow)
+class HigherConvKernel(tf.keras.Model):
+    """ Convolutional Kernel taking a quantized distribution as input."""
+def __init__(self, kernel_ensemble, channeller, commander, quantization_dim=None,distribution_dim=2, channel_dim=0, command_dim=0,name='ConvFlow'):
+    super(HigherConvKernel, self).__init__(name=name)
+    self.kernel_ensemble = kernel_ensemble
+    self.channeller = channeller
+    self.commander = commander
+    self.channel_dim = channel_dim
+    self.command_dim = command_dim
+    self.distribution_dim = distribution_dim
 
+def build_dist(self,base_distribution):
+    transformed_distribution = self.kernel_ensemble.build_dist(base_distribution)
     @tf.function
-    def _aux(self,quanta,command_batch):
-        return self.reshape(quanta[:, :1]) * self.reshape(-self.transformed_distribution.log_prob(
-            quanta[:, 1:],
-            **{'bijector_kwargs': {'command': command_batch}}
-        ))
+    def _score(sample_batch,command_batch):
+        weighted_channel_batch = self.channeller((sample_batch, command_batch))
+        # (data_batch_size,command_batch_size,infra_command_dim+1)
 
-    def pre_score(self, batch_quantized_dist):  # (batch_size,quantization_dim,dim+1)
-        batch_quantized_dist_no_coord = batch_quantized_dist[:, :, :self.channel_kernel_dim]
-        weighted_command_batch = self.channel(
-            batch_quantized_dist_no_coord)  # (data_batch_size,command_batch_size,command_dim+1)
-        for command_batch in tf.unstack(weighted_command_batch, axis=1):
-            for quanta in tf.unstack(batch_quantized_dist, axis=1):
-                print(self._aux(quanta, command_batch[:, :-1]).shape)
-                return
 
-    @tf.function
-    def score(self,batch_quantized_dist):   # (batch_size,quantization_dim,dim+1)
-        batch_size,quantization_dim,dimp1 = batch_quantized_dist.shape
-        batch_quantized_dist_no_coord = batch_quantized_dist[:,:,:self.channel_kernel_dim]
-        weighted_command_batch = self.channel(batch_quantized_dist_no_coord) # (data_batch_size,command_batch_size,command_dim+1)
+    def _prob(sample_batch,command_batch,*args,**kwargs):
+        weighted_channel_batch = self.channeller((sample_batch, command_batch))
+        # (data_batch_size,command_batch_size,command_dim+1)
         A = [
-            # (batch_size,quantization_dim,1) -> (batch_size,1)
-
+            self.reshape(transformed_distribution.prob(sample_batch, self.commander((channel_batch, command_batch)),*args, **kwargs))
+            for channel_batch in tf.unstack(weighted_channel_batch[:, :, :-1], axis=1)
         ]
-        for command in tf.unstack(weighted_command_batch[:, :, :-1], axis=1):
-            batch_quantized_dist_flat =  tf.reshape(batch_quantized_dist, (-1, batch_quantized_dist.shape[-1]))  #(None*quant_dim,dim+1)
-            command_dim=command.shape[1]
-            x = batch_quantized_dist[:,:,:1]*0+self.reshape_command(command) # (None,quant_dim,command_dim)
-            command_batch_flat = tf.reshape(x,(-1,command_dim))
-            a=tf.reduce_sum(tf.reshape(self._aux(batch_quantized_dist_flat, command_batch_flat),(-1,quantization_dim,1)),axis=1)
-            A.append(a)
-        scores = self.concat_total(A)  #(batch_size,command_dim)
-        weight_batch = weighted_command_batch[:, :, -1]
-        return self.dot([scores,weight_batch])
+        probs = self.concat(A)
+        weight_batch = weighted_channel_batch[:, :, -1]
+        return self.dot([probs, weight_batch])
 
+    def _sample(channel_command_batch,*args,**kwargs):
+        samples = transformed_distribution.sample(
+            self.commander(tf.split(channel_command_batch,[self.channel_dim,self.command_dim],axis=-1)),
+            *args,
+            **kwargs
+        )
+        return samples
 
+    return CommandedTransformedDistribution(
+        transformed_distribution,
+        _score,
+        _prob,
+        _sample,
+        command_dim=self.command_dim,
+        distribution_dim=self.distribution_dim
+    )
 
-
-    # @tf.function
-    def train_step(self, batch_quantized_dist):
-        trainable_var = self.trainable_variables
-        with tf.GradientTape() as tape:
-            scores = self.score(batch_quantized_dist)
-            loss = tf.reduce_mean(scores)
-        grad = tape.gradient(loss, trainable_var)
-        self.optimizer.apply_gradients(zip(grad, trainable_var))
-        return {'loss':loss}
+# class ConvFlowV2(tf.keras.Model):
+#     """Multi-layer NN ode_fn."""
+#     def __init__(self, flow, channel,quantization_dim, channel_kernel_dim,name='ConvFlow',
+#                  base_distribution_gen = tfd.MultivariateNormalDiag,
+#                  base_distributionKWarg = None
+#                  ):
+#         super(ConvFlowV2, self).__init__(name=name)
+#         self.flow = flow
+#         self.channel = channel
+#         self.quantization_dim = quantization_dim
+#         self.channel_kernel_dim = channel_kernel_dim
+#         self.base_distribution_gen = base_distribution_gen
+#         self.base_distributionKWarg = base_distributionKWarg
+#         self.reshape = tf.keras.layers.Reshape((1,1))
+#         self.concat_prob = tf.keras.layers.Concatenate(axis=-2)
+#         self.reshape_total = tf.keras.layers.Reshape((self.quantization_dim,1))
+#         self.concat_total = tf.keras.layers.Concatenate(axis=-1)
+#         self.reshape_command = tf.keras.layers.Reshape((1,-1))
+#         self.dot = tf.keras.layers.Dot(axes=-1)
+#         self.base_distribution = self.base_distribution_gen(**self.base_distributionKWarg)
+#         self.transformed_distribution = tfd.TransformedDistribution(distribution=self.base_distribution, bijector=self.flow)
+#
+#     @tf.function
+#     def _aux(self,quanta,command_batch):
+#         return self.reshape(quanta[:, :1]) * self.reshape(-self.transformed_distribution.log_prob(
+#             quanta[:, 1:],
+#             **{'bijector_kwargs': {'command': command_batch}}
+#         ))
+#
+#     def pre_score(self, batch_quantized_dist):  # (batch_size,quantization_dim,dim+1)
+#         batch_quantized_dist_no_coord = batch_quantized_dist[:, :, :self.channel_kernel_dim]
+#         weighted_command_batch = self.channel(
+#             batch_quantized_dist_no_coord)  # (data_batch_size,command_batch_size,command_dim+1)
+#         for command_batch in tf.unstack(weighted_command_batch, axis=1):
+#             for quanta in tf.unstack(batch_quantized_dist, axis=1):
+#                 print(self._aux(quanta, command_batch[:, :-1]).shape)
+#                 return
+#
+#     @tf.function
+#     def score(self,batch_quantized_dist):   # (batch_size,quantization_dim,dim+1)
+#         batch_size,quantization_dim,dimp1 = batch_quantized_dist.shape
+#         batch_quantized_dist_no_coord = batch_quantized_dist[:,:,:self.channel_kernel_dim]
+#         weighted_command_batch = self.channel(batch_quantized_dist_no_coord) # (data_batch_size,command_batch_size,command_dim+1)
+#         A = [
+#             # (batch_size,quantization_dim,1) -> (batch_size,1)
+#
+#         ]
+#         for command in tf.unstack(weighted_command_batch[:, :, :-1], axis=1):
+#             batch_quantized_dist_flat =  tf.reshape(batch_quantized_dist, (-1, batch_quantized_dist.shape[-1]))  #(None*quant_dim,dim+1)
+#             command_dim=command.shape[1]
+#             x = batch_quantized_dist[:,:,:1]*0+self.reshape_command(command) # (None,quant_dim,command_dim)
+#             command_batch_flat = tf.reshape(x,(-1,command_dim))
+#             a=tf.reduce_sum(tf.reshape(self._aux(batch_quantized_dist_flat, command_batch_flat),(-1,quantization_dim,1)),axis=1)
+#             A.append(a)
+#         scores = self.concat_total(A)  #(batch_size,command_dim)
+#         weight_batch = weighted_command_batch[:, :, -1]
+#         return self.dot([scores,weight_batch])
+#
+#
+#
+#
+#     # @tf.function
+#     def train_step(self, batch_quantized_dist):
+#         trainable_var = self.trainable_variables
+#         with tf.GradientTape() as tape:
+#             scores = self.score(batch_quantized_dist)
+#             loss = tf.reduce_mean(scores)
+#         grad = tape.gradient(loss, trainable_var)
+#         self.optimizer.apply_gradients(zip(grad, trainable_var))
+#         return {'loss':loss}
--- test.py
+++ test.py
@@ -11,13 +11,15 @@ now = datetime.datetime.now()
 date_time_str = now.strftime("%m-%d_%Hh%Mm%Ss")
 
 T = time.time()
-EPOCHS = 3
+EPOCHS = 150
 DISTRIBUTION_DIM = 2
 # CHANNEL_DIM = 1
 COMMAND_DIM = 0
 CHANNEL_SAMPLE = 2
 SAVE_FOLDER = os.path.join('results',date_time_str)
 os.makedirs(SAVE_FOLDER)
+model_folder = os.path.join(SAVE_FOLDER,'model')
+os.makedirs(model_folder)
 ARCH = 0
 
 
@@ -28,8 +30,8 @@ switch_arch = [
             'dim' : DISTRIBUTION_DIM,
             'ensemble_size' : 2,
             'n_switch' : 1,
-            'inward_depth' : 3,
-            'inward_width' : 16,
+            'inward_depth' : 4,
+            'inward_width' : 8,
             'switch_dim' : 8,
             'kernelKWarg' : {'activation': tf.keras.activations.tanh},
         }
@@ -49,7 +51,6 @@ switch_arch = [
 ]
 
 ffjord_core = ConvolutionalKernel.utils.build(switch_arch[ARCH])
-# ffjord_core = ConvolutionalKernel.CommandedConstructors.commanded_switched_ensemble_sequential_dense(**switch_arch2)
 infra_command = ffjord_core.command_dim
 
 FFJORD_dorpri_arch1 = {
@@ -76,8 +77,8 @@ channeller_archs = [
             'distribution_dim': DISTRIBUTION_DIM,
             'channel_dim': infra_command,
             'command_dim': COMMAND_DIM,
-            'width': 1,
-            'depth': 1,
+            'width': 8,
+            'depth': 3,
             'kernelKWarg': {'activation': tf.keras.layers.LeakyReLU()},
             # 'final_activation' : tf.keras.layers.Activation('softmax')
             'finite_set': tf.eye(infra_command)
@@ -113,12 +114,15 @@ models = [
     ConvKernel.channeller,
     ConvKernel.commander
 ]
+
 for model in models:
-    dot_img_file=os.path.join(SAVE_FOLDER,'%s.png' % model.name)
+    dot_img_file=os.path.join(model_folder,'%s_graph.png' % model.name)
     tf.keras.utils.plot_model(
         model, to_file=dot_img_file, show_shapes=True,expand_nested=True,
         show_layer_activations=True,
     )
+    with open(os.path.join(model_folder,'%s_summary.txt' % model.name),'w') as f:
+        model.summary(print_fn=lambda x:f.write(x+'\n'))
 
 
 base_distributionKWarg = {
@@ -127,42 +131,62 @@ base_distributionKWarg = {
     }
 base_distribution = tfd.MultivariateNormalDiag(**base_distributionKWarg)
 
-batch_size=2**8
-dataset_size = 2**14;dataset_size
+
 
 transformed_distribution = ConvKernel.build_dist(base_distribution)
+
 # transformed_distribution = kernel_ensemble.build_dist(base_distribution)
+mnist_list=[1,3,5,7,2,35,13,15,17,18,19]
+batch_size = 28*28
+dataset_size = 28*28
+for j in mnist_list:
+    image = tf.keras.datasets.mnist.load_data()[0][0][j]
+    xmax, ymax = image.shape
+    commands = tf.ones((*image.shape,transformed_distribution.command_dim))
+    image = image.reshape(*image.shape,1)
+    limits = [(-2,2,4/ymax),(-2,2,4/xmax)]
+    sample = np.mgrid[[slice(a, b, e) for a, b, e in limits]].transpose().astype('float32')
+    dataset_as_tensor = np.concatenate([image,sample,commands],axis=-1).reshape(xmax*ymax,-1)
+    plt.matshow(tf.reshape(dataset_as_tensor[:,0],(xmax,ymax)))
 
-weights = tf.ones((dataset_size,1))
-moons = 1.1*(skd.make_moons(n_samples=dataset_size, noise=.06)[0].astype('float32')+np.array([[-0.5,0]],dtype='float32'))
-commands = tf.ones((dataset_size,transformed_distribution.command_dim))
-dataset_as_tensor = tf.concat([weights, moons,commands],axis=1)
-dataset_as_tensor.shape
-dataset = tf.data.Dataset.from_tensor_slices(dataset_as_tensor)
-dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
-dataset = dataset.cache()
-dataset = dataset.shuffle(dataset_size)
-dataset = dataset.batch(batch_size)
-transformed_distribution.compile(
-    optimizer=tf.keras.optimizers.Adam(1e-2)
-)
+# batch_size=2**8
+# dataset_size = 2**14;dataset_size
+# weights = tf.ones((dataset_size,1))
+# moons = 1.1*(skd.make_moons(n_samples=dataset_size, noise=.06)[0].astype('float32')+np.array([[-0.5,0]],dtype='float32'))
+# commands = tf.ones((dataset_size,transformed_distribution.command_dim))
+# dataset_as_tensor = tf.concat([weights, moons,commands],axis=1)
+# dataset_as_tensor.shape
+# plt.scatter(moons[:,0],moons[:,1])
 
-plt.scatter(moons[:,0],moons[:,1])
-plt.savefig(os.path.join(SAVE_FOLDER,'target.png'))
 
-# ffjord_core(0.,moons[:batch_size],command=tf.ones((batch_size,ffjord_core.command_dim)))
-for epoch in range(EPOCHS):
-    # transformed_distribution.fit(
-    #     dataset,
-    #     epochs=1
-    # )
-    i=0
 
-    for batch in dataset:
-        L = time.time()
-        transformed_distribution.train_step(batch)
-        i+=1
-        print("epoch %s, batch %s done in %s seconds" % (epoch,i,time.time()-L))
-    transformed_distribution.display_density(name=os.path.join(SAVE_FOLDER,'epoch_%s.png' % epoch))
+    plt.savefig(os.path.join(SAVE_FOLDER,'target_%s.png' % j))
+    dataset = tf.data.Dataset.from_tensor_slices(dataset_as_tensor)
+    dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
+    dataset = dataset.cache()
+    dataset = dataset.shuffle(dataset_size)
+    dataset = dataset.batch(batch_size)
+    transformed_distribution.compile(
+        optimizer=tf.keras.optimizers.Adam(1e-2)
+    )
 
-print("done in %s seconds" % (time.time()-T))
+    #
+    #
+    # a = dataset_as_tensor.reshape(xmax,ymax,-1)
+    # a[18,27]
+
+
+
+    # transformed_distribution.score(moons[:batch_size],commands[:batch_size])
+    for epoch in range(EPOCHS):
+        i=0
+        for batch in dataset:
+            L = time.time()
+            transformed_distribution.train_step(batch)
+            i+=1
+            print("epoch %s, batch %s done in %s seconds" % (epoch,i,time.time()-L))
+        if epoch%10 == 0:
+            transformed_distribution.display_density(name=os.path.join(SAVE_FOLDER,'epoch_%03d_number_%02d.png' % (j,epoch)))
+
+
+    print("done in %s seconds" % (time.time()-T))
