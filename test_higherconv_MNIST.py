9594e9974d71517d0db478a730f409b76a9e2259
leo <poker@brunswic.fr>
2023-06-12 09:35:56 +0800

    improved test

--- test_higherconv_MNIST.py
+++ test_higherconv_MNIST.py
@@ -10,11 +10,11 @@ from PIL import Image
 import sys
 # tf.config.optimizer.set_experimental_options({'disable_meta_optimizer':True})
 
-
-GPU = 0
-gpus = tf.config.list_physical_devices('GPU')
-if gpus:
-    tf.config.set_visible_devices(gpus[GPU],'GPU')
+#
+# GPU = 0
+# gpus = tf.config.list_physical_devices('GPU')
+# if gpus:
+#     tf.config.set_visible_devices(gpus[GPU],'GPU')
 
 tfd = tfp.distributions
 now = datetime.datetime.now()
@@ -54,7 +54,9 @@ else:
     ensemble_size = 3
     # dataset_size = 28*28
     images = np.load('MNIST.npy')
-    QUANTIZATION_DIM = images[0].size
+    # QUANTIZATION_DIM = images[0].size
+    QUANTIZATION_DIM = 64
+
 
 switch_arch = [
     [
@@ -96,7 +98,7 @@ channeller_archs = [
             'distribution_shape': channeller_input_shape,
             'channel_dim': infra_command,
             'command_dim': COMMAND_DIM,
-            'channel_sample':3,
+            'channel_sample':2,
             'width':16,
             'depth':2,
             'keep':(...,-1)
@@ -143,38 +145,32 @@ base_distributionKWarg = {
 base_distribution = tfd.MultivariateNormalDiag(**base_distributionKWarg)
 transformed_distribution = ConvKernel.build_dist(base_distribution)
 
-# #
-# def gen_sample_generator(dataset_as_tensor,commands, batch_size, delta_x, delta_y, max_batch_slice_size=2**11,p=p):
-#     noise_scale = np.array([[delta_x,delta_y]])
-#     batch_slice = [batch_size]
-#     if batch_size > max_batch_slice_size:
-#         batch_slice = [max_batch_slice_size for _ in range(batch_size//max_batch_slice_size)] + [ x for x in [batch_size%max_batch_slice_size] if x > 0]
-#
-#     def gen():
-#         while True:
-#             T = time.time()
-#             samples = tf.concat(
-#                 [
-#                     tf.reshape(tf.random.categorical(tf.math.log(1e-8+val.reshape(1,-1)), batch_slice_size), (-1,))
-#                     for batch_slice_size in batch_slice
-#                 ],
-#                 axis=0
-#             )
-#             noise = tf.random.uniform([batch_size,DISTRIBUTION_DIM])*noise_scale
-#             res = tf.concat(
-#                 [
-#                     weighted_data[samples][:,:1]**(1-p),
-#                     weighted_data[samples][:,1:1+DISTRIBUTION_DIM]+noise,
-#                     weighted_data[samples][:,1+DISTRIBUTION_DIM:]
-#                 ],
-#                 axis=1
-#             )
-#             print('batch generation time:',time.time()-T)
-#             yield res
-#     return gen()
-np.broadcast_to
-image = images[0]
+
+
+def gen_sample_generator(pictures_coord,commands, batch_size, quant_dim, delta_x, delta_y, max_batch_slice_size=2**11,field_coord=-1):
+    noise_scale = np.array([[[delta_x,delta_y]]])
+    rng = np.random.default_rng()
+    indices = np.arange(pictures_coord.shape[0])
+    weights = tf.ones(batch_size,1)
+    def gen():
+        while True:
+            T = time.time()
+            batch_indices = rng.choice(indices,size=batch_size)
+            picture_batch = pictures_coord[batch_indices]
+            val = picture_batch[:,:, field_coord]
+            val = val/np.sum(val,axis=1).reshape(-1,1)
+            samples =  tf.random.categorical(tf.math.log(1e-8+val), quant_dim)
+            chosen = tf.gather(picture_batch,samples,batch_dims=1)
+
+            noise = tf.random.uniform([batch_size,quant_dim,DISTRIBUTION_DIM])*noise_scale
+            coord = chosen[:,:,:DISTRIBUTION_DIM]+noise
+            command = tf.gather(commands,batch_indices)
+            res = (weights,coord,command)
+            yield res
+    return gen()
+
 dataset_size = images.shape[0]
+image = images[0]
 image.shape
 xmax, ymax = image.shape
 commands = tf.ones((images.shape[0],transformed_distribution.command_dim))
@@ -182,21 +178,29 @@ delta_x,delta_y = 4/xmax, 4/ymax
 limits = [(-2,2,delta_y),(-2,2,delta_x)]
 picture_coord = np.mgrid[[slice(a, b, e) for a, b, e in limits]].transpose().astype('float32').reshape(1,image.size,2)
 images_flat = images.reshape(dataset_size,image.size,1)
-dataset_as_tensor = np.concatenate([np.broadcast_to(picture_coord,(dataset_size,image.size,2)),images_flat],axis=-1)
-dataset_as_tensor.shape
+pictures_coord = np.concatenate([np.broadcast_to(picture_coord,(dataset_size,image.size,2)),images_flat],axis=-1)
+pictures_coord.shape
 commands.shape
+
+
+
 for i in range(3):
-    plt.matshow(tf.reshape(dataset_as_tensor[i,:,-1],(xmax,ymax)))
+    plt.matshow(tf.reshape(pictures_coord[i,:,-1],(xmax,ymax)))
     plt.savefig(os.path.join(SAVE_FOLDER,'target_%s.png' % i))
     plt.clf()
 #
-# dataset = tf.data.Dataset.from_generator(
-#     gen_sample_generator,
-#     output_signature=tf.TensorSpec(shape=(batch_size,3),dtype=tf.float32),
-#     args=(dataset_as_tensor,batch_size,delta_x,delta_y)
-# )
-dataset = tf.data.Dataset.from_tensor_slices(dataset_as_tensor)
-dataset = dataset.batch(batch_size)
+output_signature = (
+    tf.TensorSpec(shape=(batch_size,1),dtype=tf.float32,name='weights'),
+    tf.TensorSpec(shape=(batch_size,QUANTIZATION_DIM,DISTRIBUTION_DIM+1),dtype=tf.float32,name='coordinates'),
+    tf.TensorSpec(shape=(batch_size,COMMAND_DIM),dtype=tf.float32,name='command'),
+)
+dataset = tf.data.Dataset.from_generator(
+    gen_sample_generator,
+    output_signature=output_signature,
+    args=(pictures_coord,commands,batch_size,QUANTIZATION_DIM,delta_x,delta_y)
+)
+# dataset = tf.data.Dataset.from_tensor_slices(dataset_as_tensor)
+# dataset = dataset.batch(batch_size)
 dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
 dataset = dataset.cache()
 transformed_distribution.compile(
@@ -213,10 +217,10 @@ for epoch in range(EPOCHS):
         if i > STEP_PER_EPOCH:
             break
         L = time.time()
-        transformed_distribution.train_step((tf.ones((batch_size,),dtype=tf.float32),batch,tf.zeros((batch_size,0),dtype=tf.float32)))
+        transformed_distribution.train_step(batch)
         i+=1
         print("epoch %s, batch %s done in %s seconds        " % (epoch,i,time.time()-L))
-    densities = ConvKernel.reconstruction(dataset_as_tensor[:3],tf.zeros((3,0)))
+    densities = ConvKernel.reconstruction(pictures_coord[:3],tf.zeros((3,0)))
     for i in range(3):
         plt.matshow(tf.reshape(densities[i,:],(xmax,ymax)))
         plt.savefig(os.path.join(SAVE_FOLDER,'target_%03d_epoch_%03d.png' % (i,epoch)))
